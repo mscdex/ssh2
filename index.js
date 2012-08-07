a653ab832011ca3e2ecf1818b88ef2f652c67d66
Brian White <mscdex@mscdex.net>
2012-08-07 18:53:25 -0400

    Switch from C++ binding to pure JavaScript -- too many hassles with the binding route

--- index.js
+++
@@ -1,6 +0,0 @@
-var addon = require('./build/Release/ssh2');
-var EventEmitter = require('events').EventEmitter;
-
-addon.Session.prototype.__proto__ = EventEmitter.prototype;
-
-module.exports = addon;
