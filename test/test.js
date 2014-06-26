22e2d5b8ce8ea0fa3221c0b3afcacee6d748d163
Brian White <mscdex@mscdex.net>
2014-06-26 10:15:08 -0400

    test: make test group runner async

--- test/test.js
+++ test/test.js
@@ -1,4 +1,20 @@
-require('fs').readdirSync(__dirname).forEach(function(f) {
-  if (f.substr(0, 5) === 'test-')
-    require('./' + f);
-});
+var spawn = require('child_process').spawn,
+    join = require('path').join;
+
+var files = require('fs').readdirSync(__dirname).filter(function(f) {
+      return (f.substr(0, 5) === 'test-');
+    }).map(function(f) {
+      return join(__dirname, f);
+    }),
+    f = -1;
+
+function next() {
+  if (++f < files.length) {
+    spawn('node', [ files[f] ], { stdio: 'inherit' })
+      .on('exit', function(code) {
+        if (code === 0)
+          process.nextTick(next);
+      });
+  }
+}
+next();
