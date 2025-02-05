243e193b829fd4617a9c91ac73e854d6dd6c45ce
Roman Ilin <me@romanilin.me>
2025-02-05 20:32:46 +0300

    test: bypass workers test for early Node.js versions

--- test/test-worker-imports.js
+++ test/test-worker-imports.js
@@ -5,12 +5,18 @@
 // for 4 consecutive workers.
 'use strict';
 
-const { Worker, isMainThread } = require('worker_threads');
+let Worker, isMainThread;
+try {
+  ({ Worker, isMainThread } = require('worker_threads'));
+} catch (e) {
+  if (e.code !== 'MODULE_NOT_FOUND') throw e;
+  process.exit(0);
+}
 require('../lib/index.js');
 
 if (isMainThread) {
   async function runWorker() {
-    return new Promise((r) => new Worker(__filename).on("exit", r));
+    return new Promise((r) => new Worker(__filename).on('exit', r));
   }
   runWorker()
     .then(runWorker)
